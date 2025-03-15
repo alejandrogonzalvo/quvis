be578255fd38c5f677233c5b65ce6cb4dd3798b9
Alejandro Gonzalvo <alejandro.gonzalvo.hidalgo@cern.ch>
2025-03-15 15:54:36 +0100

    readd timeline update and cast target to HTML InputElement

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { QubitGrid } from './QubitGrid.js';
 
 const grid = new QubitGrid();
 
-// const timeline = document.getElementById('timeline');
-// timeline.addEventListener('input', function(e) {
-//     const timeValue = Math.round(parseInt(e.target.value)); // Snap to nearest integer
-//     e.target.value = timeValue; // Update slider position
-//     grid.loadStateFromSlice(timeValue);
-// });
+const timeline = document.getElementById('timeline');
+timeline.addEventListener('input', function(e) {
+    const target = e.currentTarget as HTMLInputElement;
+    console.log(target.value);
+    const timeValue = Math.round(parseInt(target.value)); // Snap to nearest integer
+    target.value = String(timeValue); // Update slider position
+    grid.loadStateFromSlice(timeValue);
+});
