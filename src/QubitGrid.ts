8608d291c277c1ed785cfa5d6ec551a41762cff4
Alejandro Gonzalvo <alejandro.gonzalvo.hidalgo@cern.ch>
2025-03-22 09:50:51 +0100

    all original features are back after refactor

--- src/QubitGrid.ts
+++ src/QubitGrid.ts
@@ -7,7 +7,7 @@ import { State, States } from './State.js';
 export class QubitGrid {
     scene: THREE.Scene
     states: string[];
-    slices: Array<Map<number, Qubit>>;
+    slices: Array<Map<number, State>>;
     qubits: Map<number, Qubit>;
     mouse: THREE.Vector2;
     timeline: Timeline;
@@ -37,26 +37,26 @@ export class QubitGrid {
         this.saveCurrentState();
     }
 
-    async loadNPZData(url) {
-        const loader = new npzLoader();
-        const data = await loader.load(url);
+    // async loadNPZData(url) {
+    //     const loader = new npzLoader();
+    //     const data = await loader.load(url);
         
-        // Process each time step exactly like Spacebar generates new slices
-        const numSteps = Object.values(data)[0].length;
+    //     // Process each time step exactly like Spacebar generates new slices
+    //     const numSteps = Object.values(data)[0].length;
         
-        for (let step = 0; step < numSteps; step++) {
-            // Update all qubits for this step
-            Object.entries(data).forEach(([qubitKey, steps]) => {
-                const qubitId = parseInt(qubitKey);
-                const [x, y, z] = steps[step];
-                const state = this.xyzToState([x, y, z]);
-                this.updateQubitState(this.qubits[qubitKey]);
-            });
+    //     for (let step = 0; step < numSteps; step++) {
+    //         // Update all qubits for this step
+    //         Object.entries(data).forEach(([qubitKey, steps]) => {
+    //             const qubitId = parseInt(qubitKey);
+    //             const [x, y, z] = steps[step];
+    //             const state = this.xyzToState([x, y, z]);
+    //             this.updateQubitState(this.qubits[qubitKey]);
+    //         });
             
-            // Save as new slice exactly like generateNewSlice
-            this.saveCurrentState();
-        }
-    }
+    //         // Save as new slice exactly like generateNewSlice
+    //         this.saveCurrentState();
+    //     }
+    // }
 
     xyzToState([x, y, z]) {
         const THRESHOLD = 0.9;
@@ -68,19 +68,27 @@ export class QubitGrid {
     }
 
     saveCurrentState() {
-        this.slices.push(this.qubits);        
+        const stateSnapshot = new Map<number, State>();
+        this.qubits.forEach((qubit, id) => {
+            stateSnapshot.set(id, qubit.state);
+        });
+        this.slices.push(stateSnapshot);
         this.timeline.addSlice();
     }
 
-    loadStateFromSlice(sliceIndex) {
-        const stateSlice = this.slices[sliceIndex];
+    loadStateFromSlice(sliceIndex: number) {
+        const stateSlice = this.slices[sliceIndex - 1];
         this.timeline.setSlice(sliceIndex);
 
-        this.qubits = stateSlice;
-        this.qubits.forEach(qubit => {
-            qubit.animate();
+        // Update existing qubits instead of replacing the Map
+        this.qubits.forEach((qubit, id) => {
+            if (stateSlice.has(id)) {
+                qubit.state = stateSlice.get(id)!;
+                qubit.animate();
+            }
         });
     }
+
 
     createGrid(rows, cols) {
         const spacing = 2;
@@ -103,16 +111,17 @@ export class QubitGrid {
     }
     
     generateNewSlice() {
-        this.loadStateFromSlice(this.slices.length-1);
+        // Load previous state into CURRENT qubits
+        this.loadStateFromSlice(this.slices.length);
 
-        // Change state of 10 random qubits
+        // Modify current qubits
+        const currentIDs = Array.from(this.qubits.keys());
         for (let i = 0; i < 10; i++) {
-            const randomID: number = Math.floor(Math.random() * this.qubits.size)
-            const randomQubit: Qubit = this.qubits.get(randomID);
-            const randomState: State = States[Math.floor(Math.random() * States.length)];
-            randomQubit.state = randomState;
+            const randomID = currentIDs[Math.floor(Math.random() * currentIDs.length)];
+            const randomQubit = this.qubits.get(randomID)!;
+            randomQubit.state = States[Math.floor(Math.random() * States.length)];
         }
         
         this.saveCurrentState();
-    } 
+    }
 }
