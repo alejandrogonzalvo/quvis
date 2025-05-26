3f337733d1afd72af28e9c49d14bb9a4ddf74aaf
Alejandro Gonzalvo Hidalgo <alejandro.gonzalvo.hidalgo@cern.ch>
2025-05-26 10:21:43 +0200

    ci: Create CI to build and deploy

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/quvis/', // Replace 'quvis' with your repository name if different
+  // ... other configurations if you have them
+}) 
