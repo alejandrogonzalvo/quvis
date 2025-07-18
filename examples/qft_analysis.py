884f22a8069917e1e9a448eea66a3892b58a1326
Alejandro Gonzalvo Hidalgo <alejandro.gonzalvo.hidalgo@cern.ch>
2025-07-18 16:56:17 +0200

    fix: qft analysis decomposes qft

--- examples/qft_analysis.py
+++ examples/qft_analysis.py
@@ -6,20 +6,25 @@ from qiskit.transpiler import CouplingMap
 from quvis import QuvisVisualizer
 
 
-QUBITS = [10, 100 ]
+QUBITS = [10, 100]
 OPTIMIZATION_LEVELS = [0, 1, 2, 3]
 
 visualizer = QuvisVisualizer()
 for qubits in QUBITS:
+    circuit = QFT(qubits).decompose()
+    visualizer.add_circuit(circuit, algorithm_name=f"QFT (Q={qubits})")
+
     for optimization_level in OPTIMIZATION_LEVELS:
-        circuit = QFT(qubits)
-        coupling_map = CouplingMap.from_grid(int(ceil(sqrt(qubits))), int(ceil(sqrt(qubits))))
-        circuit = transpile(circuit, coupling_map=coupling_map, optimization_level=optimization_level)
+        coupling_map = CouplingMap.from_grid(
+            int(ceil(sqrt(qubits))), int(ceil(sqrt(qubits)))
+        )
+        circuit = transpile(
+            circuit, coupling_map=coupling_map, optimization_level=optimization_level
+        )
         visualizer.add_circuit(
-            circuit, 
+            circuit,
             coupling_map=coupling_map,
-            algorithm_name=f"QFT (Q={qubits}, O={optimization_level})"
+            algorithm_name=f"QFT (Q={qubits}, O={optimization_level})",
         )
 
 visualizer.visualize()
-
