f7be84fac6e53ce2a02b9a3717d323662fe7b4d1
Alejandro Gonzalvo <alejandrogonhid@gmail.com>
2026-02-19 09:18:01 +0100

    test: fix playground circuit generation test

--- tests/unit/playground.py
+++ tests/unit/playground.py
@@ -1,5 +1,6 @@
 import unittest
 from quvis.api.playground import PlaygroundAPI
+from quvis.config import CircuitGenerationConfig
 
 class TestPlaygroundAPI(unittest.TestCase):
 
@@ -7,20 +8,40 @@ class TestPlaygroundAPI(unittest.TestCase):
         self.api = PlaygroundAPI()
 
     def test_create_circuit(self):
-        qft_circuit = self.api._create_circuit("qft", 4)
+        qft_circuit = self.api._create_circuit(CircuitGenerationConfig(
+            algorithm="qft",
+            num_qubits=4,
+            topology="line",
+            physical_qubits=4,
+            ))
         self.assertEqual(qft_circuit.num_qubits, 4)
         self.assertTrue(qft_circuit.name.startswith("QFT"))
 
-        ghz_circuit = self.api._create_circuit("ghz", 3)
+        ghz_circuit = self.api._create_circuit(CircuitGenerationConfig(
+            algorithm="ghz",
+            num_qubits=3,
+            topology="line",
+            physical_qubits=3,
+            ))
         self.assertEqual(ghz_circuit.num_qubits, 3)
         self.assertTrue(ghz_circuit.name.startswith("GHZ"))
 
-        qaoa_circuit = self.api._create_circuit("qaoa", 4, reps=2)
+        qaoa_circuit = self.api._create_circuit(CircuitGenerationConfig(
+            algorithm="qaoa",
+            num_qubits=4,
+            topology="line",
+            physical_qubits=4,
+            ))
         self.assertEqual(qaoa_circuit.num_qubits, 4)
         self.assertTrue(qaoa_circuit.name.startswith("QAOA"))
 
         with self.assertRaises(ValueError):
-            self.api._create_circuit("invalid", 4)
+            self.api._create_circuit(CircuitGenerationConfig(
+                algorithm="invalid",
+                num_qubits=4,
+                topology="line",
+                physical_qubits=4,
+                ))
 
     def test_create_coupling_map(self):
         line_map = self.api._create_coupling_map("line", 5)
